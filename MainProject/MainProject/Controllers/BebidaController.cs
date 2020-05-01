ae806a9c903122c66282e0804f8c4a1452f79112
Eduardo Peláez <31459940+pelaezEduardo@users.noreply.github.com>
2020-05-01 15:27:58 -0600

    Laboratorio 1: Árboles Multicamino

---
+++ MainProject/MainProject/Controllers/BebidaController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MainProject.Models;
+
+namespace MainProject.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class BebidaController : ControllerBase
+    {
+        // GET: api/Bebida
+        [HttpGet]
+        public IEnumerable<Bebida> Get()
+        {
+            List<Bebida> myList = new List<Bebida>();
+            myList = DataBebida.Instance.myTree.getList();
+            myList.Sort(DataBebida.CompararBebida);
+            return myList;
+        }
+
+        // GET: api/Bebida/5
+        [HttpGet("{id}", Name = "Get")]
+        public Bebida Get(string id)
+        {
+            return DataBebida.Instance.myTree.getElement(id);
+        }
+
+        // POST: api/Bebida
+        [HttpPost]
+        public void Post([FromBody] Bebida value)
+        {
+            DataBebida.Instance.myTree.Add(value);
+        }
+    }
+}
---
+++ MainProject/MainProject/Models/DataBebida.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MainProject.BTree;
+
+namespace MainProject.Models
+{
+    public class DataBebida
+    {
+		private static DataBebida instance = null;
+		public static DataBebida Instance
+		{
+			get
+			{
+				if (instance == null) instance = new DataBebida();
+				return instance;
+			}
+		}
+
+		public BTree<Bebida, string> myTree = new BTree<Bebida, string>(5);
+
+		public static int CompararBebida(object ob1, object ob2)
+		{
+			Bebida objeto1 = (Bebida)ob1;
+			Bebida objeto2 = (Bebida)ob2;
+			return objeto1.nombre.CompareTo(objeto2.nombre);
+		}
+
+		static int buscarBebida(object nombre, object ob)
+		{
+			Bebida ob1 = (Bebida)ob;
+			string nombreObj = (string)nombre;
+			return nombreObj.CompareTo(ob1.nombre);
+		}
+
+		public DataBebida()
+		{
+			myTree.keyComparer.compareElementsDelegate(CompararBebida);
+			myTree.searchElementsDelegate(buscarBebida);
+		}
+	}
+}
